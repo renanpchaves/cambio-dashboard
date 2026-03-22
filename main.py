163a9bfc80f8e9bbd9e82a4b3b93847ecec7d796
renanpchaves <nanerpc@gmail.com>
2026-03-22 19:28:28 -0300

    🪙 feat: integration with awesomeAPI and add ExchangeService

---
+++ main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, HTTPException
+from services.exchange import (
+    exchange_service
+    )
+import requests
+
+# =================================================
+# CONFIG DA APLICAÇÃO
+# =================================================
+
+app = FastAPI(
+    title="Câmbio - Dashboard API",
+    description="API para consulta de cotação USD-BRL",
+    version="0.1.0" #beta
+)
+
+# =================================================
+# ENDPOINTS
+# =================================================
+
+@app.get("/")
+def root():
+    """
+    Endpoint root da API.
+
+    Informações básicas.
+    """
+    return{
+        "message": "API de câmbio rodando",
+        "docs_interativo": "/docs"
+    }
+
+@app.get("/exchange/cotacao")
+def get_usd_brl():
+    """
+    Retorna cotação atual USD em relação ao BRL, consumindo awesomeAPI.
+
+    1. Requisição HTTP para API externa
+    2. Valida se a resposta foi ok
+    3. Extrai dados relevantes
+    4. Retorna um JSON para o cliente
+    """
+    try:
+        return exchange_service.get_usd_brl()
+    except requests.RequestException as e:
+        raise HTTPException(
+            status_code=502,
+            detail=f"Erro ao consultar a API externa: {str(e)}"
+        )
+        
+# =================================================
+# MAIN
+# =================================================
+
+if __name__ == "__main__":
+    import uvicorn
+
+    print("""
+    ╔════════════════════════════════════════════╗
+    ║  Cambio Dashboard                          ║
+    ║                                            ║
+    ║  📖 Docs: http://localhost:8000/docs       ║
+    ╚════════════════════════════════════════════╝
+    """)
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
---
+++ services/exchange.py
@@ -0,0 +1,32 @@
+import requests
+
+class ExchangeService:
+    BASE_URL = "https://economia.awesomeapi.com.br"
+
+    def get_usd_brl(self):
+        """
+        Busca a cotação USD-BRL na awesomeAPI e retorna dados formatados.
+        """
+        url = f"{self.BASE_URL}/last/USD-BRL"
+
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+
+        data = response.json()
+        usd = data["USDBRL"]
+
+        return {
+            "code": usd["code"], #código base (USD)
+            "codein": usd["codein"], #código da moeda de destino (BRL)
+            "name": usd["name"], #nome completo do par de moedas
+            "bid": float(usd["bid"]), #preço de compra (quanto o mercado paga)
+            "ask": usd["ask"], #preço de venda (quanto o mercado cobra)
+            "high": usd["high"], #maior valor do dia
+            "low": usd["low"], #menor valor do dia
+            "pctChange": usd["pctChange"], #variação em % no dia
+            "timestamp": usd["timestamp"], #timestamp Unix da cotação
+            "create_date": usd["create_date"] #data/hora formatada da cotação
+        }
+
+#INSTANCIA DO SERVIÇO:
+exchange_service = ExchangeService()
